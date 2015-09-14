35fd98b14e2c75b329c51d082d0ac88f7a4cc846
laurensversluis <laurens_versluis@hotmail.com>
2015-09-14 23:59:24 +0100

    Small updates

--- test.py
+++ test.py
@@ -12,18 +12,20 @@ G = nx.read_shp(data_source)
 network = nx.Graph()
 
 
-
 for e in G.edges_iter():
+
     e1 = e[0]
     e2 = e[1]
-    p1 = QgsGeometry.fromWkt('Point(e1[0],e1[1])')
-    p2 = QgsGeometry.fromWkt('Point(e2[0],e2[1])')
+
+    p1 = QgsGeometry.fromPoint(QgsPoint(e1[0],e1[1]))
+    p2 = QgsGeometry.fromPoint(QgsPoint(e2[0],e2[1]))
+
     d = QgsDistanceArea()
     d.setEllipsoidalMode(True)
-    network.add_edge(e1,e2,weight=d.measureLine(QgsPoint(e1[0],e1[1]),QgsPoint(e2[0],e2[1])))
+    weight = d.measureLine(p1.asPoint(),p2.asPoint())
+    print weight
+    network.add_edge(e1,e2,weight='weight')
 
 for e in network.edges_iter():
-    print e
 
-#for e in network.edges_iter():
- #   nx.single_source_dijkstra_path_length(G,origin,e,weight='weight')
+    print nx.single_source_dijkstra_path_length(G,e[0],cutoff=None,weight='weight')
